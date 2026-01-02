cef9b8177937fb7eba829ebb776f76d6f0a7a4f2
l5cs <208353871+l5cs@users.noreply.github.com>
2026-01-02 21:57:33 +0000

    chore: dioxus example

---
+++ examples/dioxus-example/build.rs
@@ -0,0 +1,5 @@
+use scoped_css_build::compile_css;
+
+fn main() {
+    compile_css("assets/main.generated.css");
+}
---
+++ examples/dioxus-example/src/main.rs
@@ -0,0 +1,52 @@
+use dioxus::prelude::*;
+use scoped_css::{ScopedStyles, scoped_css};
+
+const CSS_ASSET: Asset = asset!("/assets/main.generated.css");
+const CSS: ScopedStyles = scoped_css!("main.css");
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div { class: &CSS["scoped"],
+            p { class: &CSS["p"], "hello css" }
+        }
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        Hero {}
+    }
+}
+
+#[component]
+fn Navbar() -> Element {
+    rsx! {
+        div { id: "navbar",
+            Link { to: Route::Home {}, "Home" }
+        }
+
+        Outlet::<Route> {}
+    }
+}
+
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[layout(Navbar)]
+    #[route("/")]
+    Home {},
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "stylesheet", href: CSS_ASSET }
+        Router::<Route> {}
+    }
+}
+
+fn main() {
+    dioxus::launch(App);
+}
